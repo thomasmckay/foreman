cbde228a29d8bd5cbfe3c40f126f43dfb9dc4402
Marek Hulan <mhulan@redhat.com>
2014-01-06 17:39:24 +0100

    New permissions for Hostgroup

--- app/controllers/hostgroups_controller.rb
+++ app/controllers/hostgroups_controller.rb
@@ -2,10 +2,9 @@ class HostgroupsController < ApplicationController
   include Foreman::Controller::HostDetails
   include Foreman::Controller::AutoCompleteSearch
 
-  before_filter :find_hostgroup, :only => [:edit, :update, :destroy, :clone]
-
   def index
-    @hostgroups = Hostgroup.my_groups.search_for(params[:search], :order => params[:order]).paginate(:page => params[:page])
+    @hostgroups = Hostgroup.authorized(:view_hostgroups).my_groups.search_for(params[:search], :order => params[:order]).paginate(:page => params[:page])
+    @authorizer = Authorizer.new(User.current, @hostgroups)
   end
 
   def new
@@ -13,7 +12,7 @@ class HostgroupsController < ApplicationController
   end
 
   def nest
-    @parent = Hostgroup.find(params[:id])
+    @parent = @hostgroup = find_hostgroup
     @hostgroup = @parent.dup
     #overwrite parent_id and name
     @hostgroup.parent_id = params[:id]
@@ -30,6 +29,7 @@ class HostgroupsController < ApplicationController
 
   # Clone the hostgroup
   def clone
+    @hostgroup = find_hostgroup
     new = @hostgroup.dup
     load_vars_for_ajax
     new.puppetclasses = @hostgroup.puppetclasses
@@ -59,12 +59,12 @@ class HostgroupsController < ApplicationController
   end
 
   def edit
-    auth  = User.current.admin? ? true : Hostgroup.my_groups.include?(@hostgroup)
-    not_found and return unless auth
+    @hostgroup = find_hostgroup(:edit_hostgroups)
     load_vars_for_ajax
   end
 
   def update
+    @hostgroup = find_hostgroup(:edit_hostgroups)
     # remove from hash :root_pass if blank?
     params[:hostgroup].except!(:root_pass) if params[:hostgroup][:root_pass].blank?
     if @hostgroup.update_attributes(params[:hostgroup])
@@ -76,6 +76,7 @@ class HostgroupsController < ApplicationController
   end
 
   def destroy
+    @hostgroup = find_hostgroup(:destroy_hostgroups)
     begin
       if @hostgroup.destroy
         process_success
@@ -99,7 +100,7 @@ class HostgroupsController < ApplicationController
 
   def process_hostgroup
 
-    @parent = Hostgroup.find(params[:hostgroup][:parent_id]) if params[:hostgroup][:parent_id].to_i > 0
+    @parent = Hostgroup.authorized(:view_hostgroups).find(params[:hostgroup][:parent_id]) if params[:hostgroup][:parent_id].to_i > 0
     return head(:not_found) unless @parent
 
     @hostgroup = Hostgroup.new(params[:hostgroup])
@@ -120,8 +121,8 @@ class HostgroupsController < ApplicationController
 
   private
 
-  def find_hostgroup
-    @hostgroup = Hostgroup.find(params[:id])
+  def find_hostgroup(permission = :view_hostgroups)
+    Hostgroup.authorized(permission).find(params[:id])
   end
 
   def load_vars_for_ajax
--- app/helpers/ancestry_helper.rb
+++ app/helpers/ancestry_helper.rb
@@ -1,7 +1,7 @@
 module AncestryHelper
 
   # this helper is used for hostgroup names and location/organization names.  Both have ancestry
-  def label_with_link(obj, max_length = 1000)
+  def label_with_link(obj, max_length = 1000, authorizer = nil)
     return if obj.blank?
     options = (obj.label.to_s.size > max_length) ? { :'data-original-title' => obj.label, :rel => 'twipsy' } : {}
     nesting = obj.label.to_s.gsub(/[^\/]+\/?$/, '')
@@ -10,7 +10,7 @@ module AncestryHelper
     link_to_if_authorized(
       content_tag(:span,
                   content_tag(:span, nesting, :class => 'gray') + name, options),
-      send("hash_for_edit_#{obj.class.name.tableize.singularize}_path", obj))
+      send("hash_for_edit_#{obj.class.name.tableize.singularize}_path", obj).merge(:auth_object => obj, :authorizer => authorizer))
   end
 
 end
